cf08cb8c6b15fcd9d5a960511b3b8a58cd8b475b
Laroub03 <ayoublaroub@gmail.com>
2023-09-28 21:49:55 +0200

    Code comments

--- src/app/auth-guard/auth.guard.ts
+++ src/app/auth-guard/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service'; // Adjust the path as needed
+import { AuthService } from '../services/auth.service'; 
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +14,14 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     
+    // Check if the user is authenticated
     const isAuthenticated = this.authService.isAuthenticatedValue;
     
     if (isAuthenticated) {
+      // If authenticated, allow access
       return true;
     } else {
-      // Redirect to the login page if user is not authenticated
+      // Redirect to the login page if the user is not authenticated
       this.router.navigate(['/login']);
       return false;
     }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,20 +11,22 @@ export class AuthService {
   private readonly registerUrl = 'https://localhost:8443/api/Auth/register'; 
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
+  // Getter to access the current authentication status
   get isAuthenticatedValue(): boolean {
     return this.isAuthenticatedSubject.value;
   }
 
-
   constructor(private http: HttpClient) {}
 
+  // Method to handle user login
   login(username: string, password: string, role: string): Observable<boolean> {
     return this.http.post(this.loginUrl, { username, password, role }, { responseType: 'text' }) 
       .pipe(
         map(response => {
           console.log('Login response:', response);
           if (response) {
-            localStorage.setItem('token', response); // Store the token in local storage
+            // Store the token in local storage and set authentication status to true
+            localStorage.setItem('token', response);
             this.isAuthenticatedSubject.next(true);
             return true;
           }
@@ -33,14 +35,14 @@ export class AuthService {
       );
   }
   
-
+  // Method to handle user registration
   register(username: string, password: string, role: string): Observable<boolean> {
     return this.http.post<{ username: string, passwordHash: string, role: string }>(
       this.registerUrl, 
       { username, password, role }
     ).pipe(
       map(response => {
-        console.log('Register response:', response); // Log the server response
+        console.log('Register response:', response); 
         if (response.username) {
           // If a username is received, registration is successful
           return true;
